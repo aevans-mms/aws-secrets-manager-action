112d9171dc111c44a057adb4c188bf0753f74e4a
Jesse Campbell <jcampbell@mms.org>
2020-05-14 17:09:16 -0400

    whyowhy

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,4 @@
 export enum Inputs {
-  AWS_ACCESS_KEY_ID = 'aws_access_key_id',
-  AWS_SECRET_ACCESS_KEY = 'aws_secret_access_key',
   AWS_REGION = 'aws_region',
   SECRETS = 'secrets',
   PARSE_JSON = 'parse_json'
